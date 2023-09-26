c6494c407a3b851d6935c62ba45fe8bfb426ad03
DanielLee34 <xurupcl1134@gmail.com>
2023-09-27 00:49:12 +0800

    add project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log(import.meta.env.VITE_TEXT);
+    }
+  }
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
